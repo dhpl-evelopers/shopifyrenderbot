f6013cb83c3bc5cd4bce6499ce5ea62e8f1f9dcc
dhpl-developers <dhpl.developers@gmail.com>
2025-07-06 14:53:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ class Config:
     # Google OAuth config
     GOOGLE_CLIENT_ID = "654156985064-vt48t8gj3qod98m4toivp6975lcdojom.apps.googleusercontent.com"
     CLIENT_SECRET = "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"
-    REDIRECT_URI = "https://shopifyrenderbot.onrender.com/?auth=google"
-
+    REDIRECT_URI = "https://shopifyrenderbot.onrender.com/"
+    
     # Azure Blob Storage config
     AZURE_CONNECTION_STRING = (
         "DefaultEndpointsProtocol=https;"
